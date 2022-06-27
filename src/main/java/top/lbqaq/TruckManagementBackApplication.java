8f4d4ebabf583fb17ab40f20ab1d98b81206886d
luoboQAQ <qqhistory@126.com>
2022-06-27 09:23:43 +0800

    init commit

---
+++ src/main/java/top/lbqaq/TruckManagementBackApplication.java
@@ -0,0 +1,13 @@
+package top.lbqaq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruckManagementBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TruckManagementBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/lbqaq/TruckManagementBackApplicationTests.java
@@ -0,0 +1,13 @@
+package top.lbqaq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruckManagementBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
